13c69eca1bfe7d904ebd56e451bba5087a08c62d
Aditya Rao Devaraj <Aditya Rao Devaraj@Aditya-PC>
2016-02-25 23:55:20 +0530

    second change

--- HelloGIT/src/com/hellogit/HelloGIT.java
+++ HelloGIT/src/com/hellogit/HelloGIT.java
@@ -5,6 +5,7 @@ public class HelloGIT {
 	public static void main(String[] args) {
 		System.out.println("Hi GIT");
 		System.out.println("First change");
+		System.out.println("Second change");
 	}
 
 }
